4821e789afb0f5efa7abe093880f3c40f94f9446
M. A. Rahul <antorahul070@gmail.com>
2021-03-05 23:03:04 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -16,6 +16,7 @@ $(document).ready(function() {
         success: function(dataResult){
           var dataResult = JSON.parse(dataResult);
           if(dataResult.statusCode==200){
+            console.log("I am insdazsxdfcgvhbjnhbgvfcdxszdxfgvhbjnhbgvfcdxsezxdfgvhbjnhbgvfcdx");
             location.href = "welcome.php";
           }
           else if(dataResult.statusCode==201){

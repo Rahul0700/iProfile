a73cf84ae6456dc6762b4b98580ece3a5d6d392d
Rahul <antorahul070@gmail.com>
2021-05-08 20:38:20 +0530

    New db creation

--- php/dbconnection_details.php
+++ php/dbconnection_details.php
@@ -1,6 +1,6 @@
 <?php
     $servername = "sql6.freemysqlhosting.net";
-    $username = "sql6410629";
-    $password = "1lnIezfZ3X";
-    $db="sql6410629";
+    $username = "sql6411126";
+    $password = "YBavJP9EqF";
+    $db="sql6411126";
  ?>
